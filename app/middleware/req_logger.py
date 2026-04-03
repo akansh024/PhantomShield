d16e794e450c15860bd6a15fafb340a4b0eb2314
Mayank Rawat <mayank.r2409@gmail.com>
2026-04-03 18:09:29 +0530

    Debug Middleware

--- app/middleware/req_logger.py
+++ app/middleware/req_logger.py
@@ -1,18 +1,21 @@
 import time
 from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
 
-async def log_requests(request: Request, call_next):
-    start_time = time.time()
 
-    response = await call_next(request)
+class RequestLoggerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
 
-    process_time = round(time.time() - start_time, 4)
+        response = await call_next(request)
 
-    ip = request.client.host
-    method = request.method
-    path = request.url.path
-    status_code = response.status_code
+        process_time = round(time.time() - start_time, 4)
 
-    print(f"[REQ] {ip} | {method} {path} | {status_code} | {process_time}s")
+        ip = request.client.host if request.client else "unknown"
+        method = request.method
+        path = request.url.path
+        status_code = response.status_code
 
-    return response
+        print(f"[REQ] {ip} | {method} {path} | {status_code} | {process_time}s")
+
+        return response
